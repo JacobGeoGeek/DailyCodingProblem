d243739301168dd73db7f96e55e29494df92f24d
Jacob Chaar <jacobchaar@live.ca>
2019-07-29 20:27:56 -0400

    First version for the Problem2

---
+++ Problem2/chanllenge2.py
@@ -0,0 +1,17 @@
+def productArray(Numbers):
+    result = []
+    Product = 1
+    for x in range(len(Numbers)):
+        for y in range(len(Numbers)):
+            if x != y:
+                Product = Product * Numbers[y]
+        result.append(Product)
+        Product = 1
+    return result
+
+
+NumbersFromProblemv1 = [1, 2, 3, 4, 5]
+NumbersFromProblemv2 = [3, 2, 1]
+
+print(productArray(NumbersFromProblemv1))
+print(productArray(NumbersFromProblemv2))
