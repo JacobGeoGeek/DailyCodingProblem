1a5a8e5eec5a790fb1845c4c8bb8f3b936132a12
JacobGeoGeek <jacobchaar@live.ca>
2020-12-28 01:19:41 -0500

    first version solution (is to make recursive function)

--- Problem34/Challenge34.py
+++ Problem34/Challenge34.py
@@ -1,2 +1,16 @@
-def find_palindrome(word):
-    pass
+def find_palindrome(word: str, numberOfletters: int = 1) -> str:
+    position: int = numberOfletters * -1
+    sub_word: str = word[position:]
+
+    new_word: str = sub_word[::-1] + word
+
+    if new_word == new_word[::-1]:
+        return new_word
+
+    numberOfletters += 1
+    return find_palindrome(word, numberOfletters)
+
+
+if __name__ == "__main__":
+    print(find_palindrome("race") == "ecarace")
+    print(find_palindrome("google") == "elgoogle")
